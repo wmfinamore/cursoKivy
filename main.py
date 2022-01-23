36b4c7585194eee1c1d09cdb5fcc182681ab2557
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-23 14:48:21 -0300

    Refactored app using scrollView and size hint

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
 
-class Tarefas(BoxLayout):
+class Tarefas(ScrollView):
     def __init__(self, tarefas, **kwargs): # key word arguments
         super().__init__(**kwargs)
         for tarefa in tarefas:
-            self.add_widget(Label(text=tarefa))
+            self.ids.box.add_widget(Label(text=tarefa, font_size=30, size_hint_y= None, height=200))
         
 
 class Test(App):
     def build(self):
-        return Tarefas(['Fazer compras', 'Buscar filho', 'Molhar plantas'], orientation='horizontal')
+        return Tarefas(['Fazer compras', 'Buscar filho', 'Molhar plantas', 'Estudar Inglês', 'Estudaqr Espanhol'])
     
 
 Test().run()
