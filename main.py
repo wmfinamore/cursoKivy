92492854913fffa88a6ed69f7e6f86d3a6ac9ec7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-22 22:09:58 -0300

    Added events and variables

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ from kivy.uix.label import Label
 class Test(App):
     def build(self):
         box = BoxLayout(orientation='vertical')
-        button = Button(text='Botao 1')
-        label = Label(text='Texto 1')
+        button = Button(text='Botao 1', font_size=30, on_release=self.incrementar)
+        self.label = Label(text='1', font_size=30)
         box.add_widget(button)
-        box.add_widget(label)
-
-        box2 = BoxLayout()
-        button2 = Button(text='Botao 1')
-        label2 = Label(text='Texto 1')
-        box2.add_widget(button2)
-        box2.add_widget(label2)
-        box.add_widget(box2)
+        box.add_widget(self.label)
         return box
 
+
+    def incrementar(self, button):
+        button.text = 'Soltei'
+        self.label.text = str(int(self.label.text)+1)
+    
 
 Test().run()
