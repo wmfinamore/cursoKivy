3c6abdb8476f4178367a45ad643b8b6abf7063f7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-23 15:16:41 -0300

    Refactored app for add widget dynamically using TextInput

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from cgitb import text
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
 
 
-class Tarefas(ScrollView):
+class Tarefas(BoxLayout):
     def __init__(self, tarefas, **kwargs): # key word arguments
         super().__init__(**kwargs)
         for tarefa in tarefas:
             self.ids.box.add_widget(Tarefa(text=tarefa))
+            
+    def addWidget(self):
+        texto = self.ids.texto.text
+        self.ids.box.add_widget(Tarefa(text=texto))
+        self.ids.texto.text = ''
         
 
 class Tarefa(BoxLayout):
