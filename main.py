d6615c8b0380739c60b4a948d041be735df1494e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-23 14:34:49 -0300

    Added widgets and parameters dynamically with kwargs

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 
-class Incrementador(BoxLayout):
-    pass
-
+class Tarefas(BoxLayout):
+    def __init__(self, tarefas, **kwargs): # key word arguments
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.add_widget(Label(text=tarefa))
+        
 
 class Test(App):
     def build(self):
-        return Incrementador()
+        return Tarefas(['Fazer compras', 'Buscar filho', 'Molhar plantas'], orientation='horizontal')
     
 
 Test().run()
