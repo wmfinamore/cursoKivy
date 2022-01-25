1fe1b3f319f0c27d5b341662fa85c4fe801bbb70
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-24 23:29:22 -0300

    Implemented back function using Android Keyboard event

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cgitb import text
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
 
 class Gerenciador(ScreenManager):
@@ -18,6 +19,21 @@ class Tarefas(Screen):
         # for tarefa in tarefas:
         #     self.ids.box.add_widget(Tarefa(text=tarefa))
             
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.voltar)
+        
+    def voltar(self, window, key, *args):
+        if key == 27:
+            App.get_running_app().root.current= 'menu'
+            return True
+        """ Código para capturar o evento do teclado
+        print(key)
+        return True
+        """
+    
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.voltar)
+    
     def addWidget(self):
         texto = self.ids.texto.text
         self.ids.box.add_widget(Tarefa(text=texto))
